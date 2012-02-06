e64c6ff23c8ba16a7d7323f147d660a2d12cefc4
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-06 21:59:55 +0800

    n

--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -1,9 +1,18 @@
 public class TowersOfHanoi {
 	public static String[] move(int num, int startPole, int endPole) {
+		if (num == 0) {
+			return null;
+		}
+		int intermediatePole = 6 - startPole - endPole;
+		move(num - 1, startPole, intermediatePole);
+		move(1, startPole, endPole);
+		// System.out.println("move " + num + "from " + startPole + "to "
+		//		+ endPole);
+		move(num - 1, intermediatePole, endPole);
 		return null;
 	}
 
 	public static void main(String[] args) {
-		move(5, 1, 3);// move 5 disks from pole 1 to pole 3
+		move(3, 1, 3);// move 5 disks from pole 1 to pole 3
 	}
 }
