c8ee01200c515fc338020d7ebc09db75506d929c
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-06 22:53:23 +0800

    n

--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 
 public class TowersOfHanoi {
 
+	public static void main(String[] args) {
+		for (String str : move(5, "a", "c")) {
+			System.out.println(str);
+		}
+	}
+
 	public static ArrayList<String> move(int num, String startPole,
 			String endPole) {
 		ArrayList<String> result = new ArrayList<String>();
@@ -13,11 +19,5 @@ public class TowersOfHanoi {
 		result.add("move " + num + " from " + startPole + " to " + endPole);
 		result.addAll(move(num - 1, "b", endPole));
 		return result;
-	}
-
-	public static void main(String[] args) {
-		for (String str : move(5, "a", "c")) {
-			System.out.println(str);
-		}
 	}
 }
