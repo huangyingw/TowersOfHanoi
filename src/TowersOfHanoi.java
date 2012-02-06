cc72165d6fad5e73b6b87335234a646c419aae68
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-06 21:48:10 +0800

    n

---
+++ src/TowersOfHanoi.java
@@ -0,0 +1,9 @@
+public class TowersOfHanoi {
+	public static String[] move(int num, int startPole, int endPole) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		move(5, 1, 3);// move 5 disks from pole 1 to pole 3
+	}
+}
