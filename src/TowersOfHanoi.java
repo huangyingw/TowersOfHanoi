7643b92cc638eedb11e0103e08710ffdb8b3edfd
huangyingw@linux_programming <huangyingw@gmail.com>
2012-02-22 17:00:07 -0600

    n

--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -2,15 +2,13 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class TowersOfHanoi {
-
 	public static Hashtable<Integer, String> numbers;
 
 	public static void main(String[] args) {
-		Hashtable<Integer, String> numbers = new Hashtable<Integer, String>();
+		numbers = new Hashtable<Integer, String>();
 		numbers.put(1, "a");
 		numbers.put(2, "b");
 		numbers.put(3, "c");
-		TowersOfHanoi.numbers = numbers;
 		System.out.println(move(5, 1, 3).size());
 		for (String str : move(5, 1, 3)) {
 			System.out.println(str);
@@ -19,7 +17,6 @@ public class TowersOfHanoi {
 
 	public static ArrayList<String> move(int num, int startPole, int endPole) {
 		ArrayList<String> result = new ArrayList<String>();
-
 		int intermediate = 6 - startPole - endPole;
 		if (num == 1) {
 			return result;
